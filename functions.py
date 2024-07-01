bf075a36d76f7b725360e25d2df4a60b01588898
kayla tapiador <kaylatapiador@yahoo.com>
2024-07-01 13:03:00 -0700

    part 4 (hw)

--- functions.py
+++ functions.py
@@ -1,39 +1,39 @@
-def name_args(**kwargs):
-    for k in kwargs.keys():
-        print(f"{k}:{kwargs[k]}")
+def max_num(a,b,c):
+    return max([a,b,c])
 
-def all_true(iter):
-    return all(iter)
+def multi_list(list):
+    if(len(list)==0):
+        return 0
+    prod = list[0]
 
-def one_true(iter):
-    return any(iter)
+    if len(list)>1:
+        for i in list[1:]:
+            prod = prod*i
+    return prod
 
-def recursive_factorial(n):
-    if n<=1:
-        return 1
-    else:
-        return n*recursive_factorial(n-1)
+def rev_string(string):
+    return string[::-1]
 
-def recursive_deduplicate(str,i=0):
-    if len(str) <=1 or i==len(str)-1:
-        return str
-    else:
-        return recursive_deduplicate(str,i+1)
+def num_within(x,a,b):
+    return x in range(a,b+1)
+
+def pascal(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    for row in triangle:
+        print(row)
 
 
-def recursive_reverse(str, i=0):
-    if len(str)==0:
-        return ""
-    elif i ==len(str)-1:
-        return str[0]
-    else:
-        return str[-1-i] + recursive_reverse(str,i+1)
-
-name_args(a=1, b=2, c=3) 
-print(all_true([1, 2, 3, 4]))  
-print(all_true([1, 0, 3, 4])) 
-print(one_true([0, 0, 0, 1]))  
-print(one_true([0, 0, 0, 0])) 
-print(recursive_factorial(5))  
-print(recursive_deduplicate("AABBCCDD"))  
-print(recursive_reverse("hello"))  
+print(max_num(1, 2, 3))        
+print(multi_list([1, 2, 3, 4]))
+print(rev_string("hello"))     
+print(num_within(3, 2, 4))     
+print(num_within(3, 1, 3))     
+print(num_within(10, 2, 5))
+pascal(5)
