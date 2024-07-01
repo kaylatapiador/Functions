3465471787846d0d6be91984ed258c65c07ab1e8
kayla tapiador <kaylatapiador@yahoo.com>
2024-07-01 00:31:46 -0700

    part 2 activity

--- functions.py
+++ functions.py
@@ -1,31 +1,47 @@
-def say_hello():
-    print("Hello!")
+def arb_args(*args):
+    for arg in args:
+        print(arg)
 
-def sum(a,b):
-    return a+b
+def inner_func(a,b):
+    def divide():
+        return a/b
+    
+    def multiply():
+        return a * b
+    
+    results = divide() + multiply()
+    print(results)
 
-def avg(a,b):
-    return (a+b)/2
+def lunch_lady(name, lunch="Mystery Meat"):
+    print(name, lunch)
 
-def name(first_name, last_name):
-    return f"{last_name}, {first_name}"
+def sum_n_product(a,b):
+    return a+b, a*b
+
+alias_arb_args = arb_args
+
+def arb_mean(*args):
+    total = 0
+    for a in args:
+        total += a
+    print(a/len(args))
+
+def arb_longest_string(*args):
+    long = 0
+    longest = ""
+    for a in args:
+        if len(a) >long:
+            long = len(a)
+            longest = a
+    return longest
 
 
-def list(first, last, year, student_num):
-    return[first, last, year, student_num]
-
-def over_18(a):
-    return a > 18
-
-def reverse(word):
-    print(word[::-1])
-
-
-say_hello()
-print(sum(3,4))
-print(avg(6,2))
-print(name("Kayla", "Tapiador"))
-print(list("Kayla", "Tapiador", 2015, 123456))
-print(over_18(17))
-print(over_18(25))
-reverse("ice")
+arb_args(1, 2, 3, "hello")  
+inner_func(3, 4)           
+lunch_lady("Benny Bill", "Pizza")      
+lunch_lady("Lebron James")              
+sum_result, product_result = sum_n_product(3, 4)
+print(sum_result, product_result)    
+alias_arb_args("a", "b", "c")       
+arb_mean(1, 2, 3, 4, 5)              
+print(arb_longest_string("apple", "banana", "cherry"))  
